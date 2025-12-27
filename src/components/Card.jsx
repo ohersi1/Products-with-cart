c30e7528789face561634080064c2d80062472b2
Osman Hersi <osmanhersi@hotmail.co.uk>
2025-12-27 03:07:15 +0000

    added all the hover states for all my buttons and fixed img issues by importing svgs

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-import cartIcon from "../assets/images/icon-add-to-cart.svg";
-import decrementImg from "../assets/images/icon-decrement-quantity.svg";
-import incrementImg from "../assets/images/icon-increment-quantity.svg";
 import "./Card.css";
 
 const Card = ({ product, decrement, increment, count }) => {
@@ -18,15 +15,18 @@ const Card = ({ product, decrement, increment, count }) => {
           <img src={images.mobile} alt={title} className={count > 0 ? "dessert_img active" : "dessert_img"} />
         </picture>
         {count === 0 ? (
-          <button onClick={() => increment(id)}>
-            <img src={cartIcon} alt="" />
+          <button className="add-to-cart-btn" onClick={() => increment(id)}>
+            
+            <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewBox="0 0 21 20"><g fill="currentColor" clipPath="url(#a)"><path d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z"/><path d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z"/></g><defs><clipPath id="a"><path fill="currentColor" d="M.333 0h20v20h-20z"/></clipPath></defs></svg>
             <span className="text-preset-4-bold">Add to Cart</span>
           </button>
         ) : (
           <button className="btn-2">
-            <img onClick={() => decrement(id)} src={decrementImg} alt="" />
+            <svg onClick={() => decrement(id)} xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewBox="0 0 10 2"><path fill="currentColor" d="M0 .375h10v1.25H0V.375Z"/></svg>
+            {/* <img className="decrement-btn" onClick={() => decrement(id)} src={decrementImg} alt="" /> */}
             <span className="text-preset-4-bold">{count}</span>
-            <img onClick={() => increment(id)} src={incrementImg} alt="" />
+            {/* <img className="increment-btn" onClick={() => increment(id)} src={incrementImg} alt="" /> */}
+            <svg onClick={() => increment(id)} xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewBox="0 0 10 10"><path fill="currentColor" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/></svg>
           </button>
         )}
       </div>
